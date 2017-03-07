6bca73696b6368697944a85ad0f1e67ee3062d8e
Greg Harrison <greg@Gregs-MacBook-Pro.local>
2017-03-07 11:08:51 -0500

    Bot will not respond to another bot, or itself

--- bot.js
+++ bot.js
@@ -14,7 +14,13 @@ client.Dispatcher.on(Events.GATEWAY_READY, e => {
 })
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-    if(e.message.content === 'PING') {
-        e.message.channel.sendMessage('PONG')
+    if (!e.message.author.bot) {
+        if(e.message.content === '!PING') {
+            e.message.channel.sendMessage('PONG')
+        }
+        if(e.message.content === '!test') {
+            console.log(e.message)
+            e.message.channel.sendMessage(e.message.content)
+        }
     }
 })
